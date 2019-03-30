43a070f2b6542c3ba93817e6cce8ab45e17fde8d
Toad Sage <kylenewins@gmail.com>
2019-03-30 13:27:21 -0400

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import cards from "./cards.json";
+import Wrapper from "./components/Wrapper";
+import Counter from "./components/Counter"
+import Card from "./components/Card"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+class App extends React.Component{
+  state = {cards}
+
+  // shuffle = id =>{
+  //   const friends = this.state.friends.filter(friend => friend.id !== id)
+  
+  //   this.setState({friends})
+  // }
+
+  clickHandle = function(){
+    console.log("click received")
+  }
+    render(){
+      return(
+  
+        <Wrapper>
+          {this.state.cards.map(single=> (
+            <Card
+            // shuffle={this.shuffle}
+            clickHandle={this.clickHandle}
+            id={single.id}
+            key={single.id}
+            name={single.name}
+            image={single.image}
+            />
+          ))}
+        </Wrapper>
+      )
   }
 }
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./style.css"
+
+function Card(props){
+    return(
+        <button className="charCard" onClick={() => props.clickHandle()}>
+        <div className="img-container">
+          <img
+            alt={props.name}
+            src = {props.image}
+          />
+        </div>
+      </button>
+    )
+
+}
+
+export default Card
